77881d9db9b4709c27c74c2020dbe97e65d0ab4f
Michał <kaviloss1@gmail.com>
2017-05-23 07:12:45 +0200

    Moved the code responsible for creating array with months to outside document month.php.

--- osczednosci/index.php
+++ osczednosci/index.php
@@ -8,31 +8,6 @@
     </head>
     
     <body>
-     <?php
-    $month = date("n");
-
-    for($i=$month; $i<=12; $i++){
-        $mies[$i] = $i;
-    }
-
-    $ile = count($mies);
-
-    for($i=$month; $i<12; $i++){
-        if($mies[$i]==1) $mies[$i]="Styczeń";
-        elseif($mies[$i]==2) $mies[$i] = "Luty";
-        elseif($mies[$i]==3) $mies[$i] = "Marzec";
-        elseif($mies[$i]==4) $mies[$i] = "Kwiecień";
-        elseif($mies[$i]==5) $mies[$i] = "Maj";
-        elseif($mies[$i]==6) $mies[$i] = "Czerwiec";
-        elseif($mies[$i]==7) $mies[$i] = "Lipiec";
-        elseif($mies[$i]==8) $mies[$i] = "Sierpień";
-        elseif($mies[$i]==9) $mies[$i] = "Wrzesień";
-        elseif($mies[$i]==10) $mies[$i] = "Październik";
-        elseif($mies[$i]==11) $mies[$i] = "Listopad";
-        elseif($mies[$i]==12) $mies[$i] = "Grudzień";
-        
-    }
-    ?>
         <header>Kalkulator oszczędności</header>
         
         <section>
@@ -42,11 +17,11 @@
                     Podaj swoje miesięczne wynagrodzenie: <input type="text" name="pensja"><br/><br/>
                     Podaj stałe miesięczne wydatki (bilet miesięczny, rachunki itp.): <input type="text" name="stale"><br/><br/>
                     <?php
-                        for($i=$month; $i<12; $i++){
+                        include('month.php');
+                        for($i=$month; $i<=12; $i++){
                         echo"Wprowadź planowane wydatki na miesiąc <b>$mies[$i]</b>: ";
                         echo"<input class='pole' type='text' name='$mies[$i]'><br/><br/>";
                         }
-                    var_dump($mies);
                     ?>
                    <input type="submit" value="Oblicz">
                 </form>
