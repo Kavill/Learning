b58d9aedfc149e04a9db3e0d9974fb7776f4b1e4
Michał <kaviloss1@gmail.com>
2017-12-14 14:52:47 +0100

    Added selectData method.

--- CMS/CLASS/DatabaseManager.class.php
+++ CMS/CLASS/DatabaseManager.class.php
@@ -54,6 +54,56 @@ class DatabaseManager{
         mysqli_close($conn);
     }
     
+    static public function SelectData($TABLE, $COLUMNS = Array("*"), $WHERE = Array(), $LOGIC_OPER = "=", $OPER = "AND"){
+        $conn = self::getConnection();
+        $SQL = "SELECT ";
+        if(count($COLUMNS) == 1){
+            $SQL .= $COLUMNS[0];
+        }
+        else{
+            foreach($COLUMNS as $column){
+                $SQL .= $column.",";
+            }
+        }
+        
+        $SQL = rtrim($SQL, ',');
+        $SQL .= " FROM {$TABLE}";
+        
+        if(count($WHERE) > 0){
+            $SQL .= " WHERE ";
+            
+            foreach($WHERE as $key => $val){
+                $SQL .= $key.$LOGIC_OPER."'".$val."' ".$OPER." ";
+            }
+            
+            $SQL = substr($SQL, 0, strlen($SQL)-(strlen($OPER)+2));
+        }
+        $result = $conn->query($SQL);
+        
+        
+        if(!$result){
+           LogFile::AddLog("Wystąpił błąd połączenia z bazą danych!", __LINE__, __FILE__); 
+           return false;
+        }
+        else{
+            $resultArray = Array();
+            while(($row = $result->fetch_array(MYSQLI_ASSOC)) !== NULL){
+                $resultArray[] = $row;
+                
+            }
+        }
+        
+        if(count($resultArray) > 0){
+            return $resultArray;
+        }
+        else{
+            LogFile::AddLog("Zapytanie bazodanowe zwróciło pusty wynik!", __LINE__, __FILE__);
+            return false;
+        }
+        
+        mysqli_close($conn);
+    }
+    
 }
 
 ?>
